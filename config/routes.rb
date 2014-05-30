012eab1acade2056f3adf175d83152576bd3c80f
Armando <SocialDist@gmail.com>
2014-05-30 07:12:45 -0600

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
-  resources :users
-  resources :sessions,   only: [:new, :create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
@@ -9,15 +14,6 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  # match '/', to: 'static_pages#home', via: 'get'
-  # get "static_pages/home"
-  # get "static_pages/help"
-  # get "static_pages/about"
-  # get "static_pages/contact"
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,14 +17,15 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
-      it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
-
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
       end
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
     end
+
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
@@ -36,12 +37,14 @@ describe "Authentication" do
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
+
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
     end
   end
+
   describe "authorization" do
 
     describe "for non-signed-in users" do
@@ -55,19 +58,6 @@ describe "Authentication" do
           click_button "Sign in"
         end
 
-        describe "in the Microposts controller" do
-
-          describe "submitting to the create action" do
-            before { post microposts_path }
-            specify { expect(response).to redirect_to(signin_path) }
-          end
-
-          describe "submitting to the destroy action" do
-            before { delete micropost_path(FactoryGirl.create(:micropost)) }
-            specify { expect(response).to redirect_to(signin_path) }
-          end
-        end
-
         describe "after signing in" do
 
           it "should render the desired protected page" do
@@ -76,12 +66,32 @@ describe "Authentication" do
         end
       end
 
-      describe "in the Users controller" do
-
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_title('Sign in') }
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
         end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Users controller" do
 
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
@@ -92,6 +102,33 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
       end
     end
 
@@ -112,16 +149,4 @@ describe "Authentication" do
       end
     end
   end
-  describe "as non-admin user" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:non_admin) { FactoryGirl.create(:user) }
-
-    before { sign_in non_admin, no_capybara: true }
-
-    describe "submitting a DELETE request to the Users#destroy action" do
-      before { delete user_path(user) }
-      specify { expect(response).to redirect_to(root_url) }
-    end
-  end
 end
-
